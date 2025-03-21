60be2c9d21332f162de39508e8e4a2e19f69282d
papsoking <papefadiaba728@gmail.com>
2025-03-21 01:27:57 +0000

    Modification sur le fichier vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()], // Ajoutez des options si nécessaire, par exemple : react({ fastRefresh: false })
   server: {
-    host: "0.0.0.0", // Écoute sur toutes les interfaces réseau
-    port: 3000, // Port utilisé pour le développement
-    strictPort: true, // Force l'utilisation du port 3000
+    allowedHosts: [
+      "easyservice-29e5.onrender.com",
+      "www.easyservice-29e5.onrender.com",
+      "localhost", // Autoriser localhost pour le développement local
+    ],
   },
-  // Utilisez un fichier .env pour les variables d'environnement
-  // Vite expose automatiquement les variables préfixées par VITE_ via import.meta.env
 });
