2e6b85c6c41d2c8208cae4c69df818e02d18ac88
abdoulaye-96 <107267177+abdoulaye-96@users.noreply.github.com>
2025-03-29 06:00:19 +0100

    dynamiser contact

--- src/pages/client/ServiceDetail.jsx
+++ src/pages/client/ServiceDetail.jsx
@@ -63,14 +63,14 @@ const ServiceDetail = () => {
         </ul>
       </div>
 
-      <div className="flex space-x-4">
+      {/* <div className="flex space-x-4">
         <button className="bg-red-500 text-white px-4 py-2 rounded-md">
           Supprimer
         </button>
         <button className="bg-blue-500 text-white px-4 py-2 rounded-md">
           Modifier
         </button>
-      </div>
+      </div> */}
     </div>
   );
 };
