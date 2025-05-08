13c0b3e69de3fac438188e57626a401714cbbc77
papsoking <papefadiaba728@gmail.com>
2025-05-08 20:30:05 +0000

    Gestion des erreurs de vercel  pour la redirection

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vercel from "vite-plugin-vercel";
 
 export default defineConfig({
   plugins: [react(), vercel()],
+  base: "/",
   server: {
     host: "0.0.0.0", // Écoute sur toutes les interfaces réseau
     port: 3000, // Port utilisé pour le développement
