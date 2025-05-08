15c4a6df8fe151dd52b667f047c356374f3ec176
papsoking <papefadiaba728@gmail.com>
2025-05-08 17:51:20 +0000

    Déploiement avec vercel

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
     host: "0.0.0.0", // Écoute sur toutes les interfaces réseau
     port: 3000, // Port utilisé pour le développement
     strictPort: true, // Force l'utilisation du port 3000
-    allowedHosts: ["easyservice-29e5.onrender.com", "easyservice-frontend-l01x.onrender.com", "easyservice-zhmc.onrender.com"], // Autoriser ces hôtes
+    allowedHosts: ["easyservice-29e5.onrender.com", "easyservice-frontend-l01x.onrender.com", "easyservice-zhmc.onrender.com", "easyservice.vercel.app"], // Autoriser ces hôtes
   },
 });
